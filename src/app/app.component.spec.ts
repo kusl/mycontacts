18f19950d05d88a86b3d4d4adefe33d636e615f2
Kushal <kushaldeveloper@gmail.com>
2024-11-22 06:37:03 -0500

    change my contacts to My Contacts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mycontacts');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, My Contacts');
   });
 });
