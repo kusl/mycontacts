f6119ca893e6e77cb8884e0fab1d3596a838948c
Kushal <kushaldeveloper@gmail.com>
2024-11-22 19:47:47 -0500

    fix failing test
    yarn run v1.22.22
    $ ng test
    ✔ Browser application bundle generation complete.
    22 11 2024 19:45:26.681:INFO [karma-server]: Karma v6.4.4 server started at http://localhost:9876/
    22 11 2024 19:45:26.685:INFO [launcher]: Launching browsers FirefoxHeadless with concurrency unlimited
    22 11 2024 19:45:26.701:INFO [launcher]: Starting browser Firefox
    22 11 2024 19:45:30.812:INFO [Firefox 128.0 (Linux x86_64)]: Connected on socket 3n3tvJASeuTBPf6LAAAB with id 99146495
    Firefox 128.0 (Linux x86_64) AppComponent should have the 'mycontacts' title FAILED
            Expected 'My Contacts' to equal 'mycontacts'.
            <Jasmine>
            1675/</<@main.js:34:23
            <Jasmine>
    Firefox 128.0 (Linux x86_64): Executed 3 of 3 (1 FAILED) (0 secs / 0.507 secs)
    Firefox 128.0 (Linux x86_64) AppComponent should have the 'mycontacts' title FAILED
            Expected 'My Contacts' to equal 'mycontacts'.
            <Jasmine>
            1675/</<@main.js:34:23
    Firefox 128.0 (Linux x86_64): Executed 3 of 3 (1 FAILED) (0.535 secs / 0.507 secs)
    TOTAL: 1 FAILED, 2 SUCCESS
    TOTAL: 1 FAILED, 2 SUCCESS
    error Command failed with exit code 1.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mycontacts' title`, () => {
+  it(`should have the 'My Contacts' title by default`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mycontacts');
+    expect(app.title).toEqual('My Contacts');
   });
 
   it('should render title', () => {
